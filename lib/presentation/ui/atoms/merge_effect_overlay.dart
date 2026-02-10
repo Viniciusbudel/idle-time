886d11f080a9f495b66f8a36374b5f2551ba7187
Vinicius Budel <viniciuhbudel@gmail.com>
2026-02-10 13:24:20 -0300

    refactor: move lib/presentation/ui/widgets/merge_effect_overlay.dart to lib/presentation/ui/atoms/merge_effect_overlay.dart

---
+++ lib/presentation/ui/atoms/merge_effect_overlay.dart
@@ -0,0 +1,35 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:time_factory/presentation/anim/merge_effect_game.dart';
+
+class MergeEffectOverlay extends StatefulWidget {
+  final VoidCallback onComplete;
+  final Color primaryColor;
+
+  const MergeEffectOverlay({
+    super.key,
+    required this.onComplete,
+    this.primaryColor = Colors.white,
+  });
+
+  @override
+  State<MergeEffectOverlay> createState() => _MergeEffectOverlayState();
+}
+
+class _MergeEffectOverlayState extends State<MergeEffectOverlay> {
+  late MergeEffectGame _game;
+
+  @override
+  void initState() {
+    super.initState();
+    _game = MergeEffectGame(
+      onComplete: widget.onComplete,
+      primaryColor: widget.primaryColor,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(child: GameWidget(game: _game));
+  }
+}
